bd12635273302abbc5fbb665b83f27e0226a0bda
tonyy kosseigyy <tony kosseify>
2021-07-26 18:02:56 +0300

    Creating Info component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./App.css";
 import Header from './Components/Header' ;
-import Info from './Components/Info' ;
 
 
 const App = () => {
   return (
     <div className='app'>
       <Header />
-      <Info />
     </div>
   );
 }
