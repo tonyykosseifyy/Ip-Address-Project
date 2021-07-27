92a7f618399b857445aa5ca19d2d8bbccd49a213
tonyy kosseigyy <tony kosseify>
2021-07-27 21:22:52 +0300

    Fly to functionality from a location to another with animation

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,24 +1,23 @@
 import React,{ useEffect , useState } from 'react' ;
 import './Body.css' ;
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet' ;
+import { MapContainer, TileLayer, Marker, Popup , useMapEvents} from 'react-leaflet' ;
 
 
 const Body = ({ data }) => {
-  const loc = [51.505, -0.09] ;
-  const [ position , setPosition ] = useState(loc) ;
-  console.log('body data ' , data)
-  console.log('center=> ' , data.hasOwnProperty('lat') ? [data.lat , data.lng] : position)
-  console.log(position)
+  const position = [51.505, -0.09] ;
+  const [ map , setMap ] = useState(null) ;
+  console.log(map) ;
   useEffect(() => {
-    if ( data.hasOwnProperty('lat') ) {
-      setPosition([data.lat , data.lng])
-    } else {
-      setPosition(loc)
+    if (map) {
+      const lastCenter = map.options.center ;
+      if ((lastCenter[0] !== data.lat) || (lastCenter[1] !== data.lng) ) {
+        map.flyTo([data.lat , data.lng ], map.getZoom())
+      }
     }
   },[ data ])
   return (
     <div className='body'>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+      <MapContainer whenCreated={setMap} center={data.hasOwnProperty('lat') ? [data.lat , data.lng] : position} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
